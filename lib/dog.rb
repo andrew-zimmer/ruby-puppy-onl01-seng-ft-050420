9d9f3ea74f6317f7dd0bb6c641c1e7e8b193e7d4
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-13 13:13:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,20 @@
 class Dog 
   @@all = []
   
+  attr_accessor :all 
+  
   def initialize(name)
-    @@all << name 
+    @name = name 
+  end 
+  
+  def save
+    @@all.push(@name)
   end 
   
   def self.all 
     @@all 
+  end 
+  def print_all 
+    @@all.all {|pup| puts "#{pup}"}
   end 
 end 
